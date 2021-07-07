43ecbe6f3a5924d7d8995a9eca68ecda32a9ca85
PavloMartovichPowerSnow <mryaremchyk@gmail.com>
2021-07-07 16:40:43 +0300

    Add alternative avatar.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 const val = document.getElementsByClassName('value');
+const avatar = document.getElementById('avatar');
 const birthday = new Date(1996, 11, 18);
 
 val[2].innerHTML = parseInt((Date.now() - birthday)/(31536000000));
+
+const changeAva = () => {
+    avatar.setAttribute('src', 'images/alter_avatar.png');
+}
+
+const resetChange = () => {
+    avatar.setAttribute('src', 'images/avatar.png');
+}
+
+avatar.addEventListener('mouseover', changeAva);
+avatar.addEventListener('mouseout', resetChange);
